cde9574ebb71fb508868d3404833b35a7afb7da2
Amanda Luby <lubyamanda@gmail.com>
2026-09-09 13:24:07 -0500

    f26 updates

--- dates.R
+++ dates.R
@@ -1,5 +1,11 @@
-mondays = mdy("3/30/2026") + weeks(0:9)
-wednesdays = mdy("4/1/2026") + weeks(0:9)
-fridays = mdy("4/3/2026") + weeks(0:9)
+library(tidyverse)
 
+mondays = mdy("9/14/2026") + weeks(0:9)
+wednesdays = mdy("9/16/2026") + weeks(0:9)
+fridays = mdy("9/18/2026") + weeks(0:9)
+
+# highlight this and then addins --> value to clipboard
 as.character(stamp("Mar 30, 2026")(sort(c(mondays, wednesdays, fridays))))
+
+
+
